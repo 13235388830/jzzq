7f4f5ee2267f7553d70f26ca3f313349385026ea
wujirui <757173734@qq.com>
2018-12-26 17:42:36 +0800

    commit

--- app/src/main/java/com/ami/makemoneyby/SplashActivity.java
+++ app/src/main/java/com/ami/makemoneyby/SplashActivity.java
@@ -78,6 +78,7 @@ public class SplashActivity extends BaseActivity {
                                     editor.putBoolean("isFirst",true);
                                     editor.commit();
                                     startActivity(new Intent(SplashActivity.this, SplashSecondActivity.class));
+                                    //1111
                                     finish();
                                 }
                             }
